0741ae160fa3bd0533c7e443f9ea302c84c69ff4
hata4ma <109394487+hata4ma@users.noreply.github.com>
2025-01-25 09:01:38 +0900

    Update settings.py

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bookproject.wsgi.application'
 
 # For Render add 1 line
 import dj_database_url
-allowed hosts = ['*']
+# allowed hosts = ['*']
 # allowed_hosts ?
 # allowed_hosts = ['*']
 
